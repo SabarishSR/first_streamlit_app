f0938257dd4b1b0dbb3e8135e000c4999544cd65
SabarishSR <153103929+SabarishSR@users.noreply.github.com>
2023-12-21 10:16:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,16 +36,3 @@ else:
 
 
 
-
-except YRLError as e:
-streamlit.error()
-
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
